c0be0ef1d24cc3d396221d1c31812f1fa4a5d90d
Fizioh <batrom@laposte.net>
2024-03-17 12:38:46 +0100

    feat: Add variant styles and stories for Button component

--- simple-ui/src/components/Button/index.stories.tsx
+++ simple-ui/src/components/Button/index.stories.tsx
@@ -14,9 +14,27 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
+export const Solid: Story = {
     args: {
         children: "Click me",
-
+        variant: "solid",
+        size: "md",
+        colorscheme: "primary",
+    }
+};
+export const Outline: Story = {
+    args: {
+        children: "Click me",
+        variant: "outline",
+        size: "md",
+        colorscheme: "primary",
+    }
+};
+export const Ghost: Story = {
+    args: {
+        children: "Click me",
+        variant: "ghost",
+        size: "md",
+        colorscheme: "primary",
     }
 };
